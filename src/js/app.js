17f015aab618e51949271de40fe3cb5b34c46ec8
kyleschaeffer <kyle@spirito.us>
2019-06-04 15:46:48 -0400

    Create a reusable component via querySelectorAll iterator

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,23 @@
-// Get the taco button
-const tacoBtn = document.getElementById('taco-btn')
-
-// Button click listener
-tacoBtn.addEventListener('click', e => {
-  // Prevent default click
-  e.preventDefault()
-
-  // Spin that taco
-  tacoBtn.classList.toggle('spin')
-})
-
-// Button key listener
-tacoBtn.addEventListener('keydown', e => {
-  // Key: t
-  if (e.key === 't') {
-    // Prevent default keydown
+// Iterate all spin buttons
+document.querySelectorAll('.spin-btn').forEach(btn => {
+  // Button click listener
+  btn.addEventListener('click', e => {
+    // Prevent default click
     e.preventDefault()
 
     // Spin that taco
-    tacoBtn.click()
-  }
+    btn.classList.toggle('spin')
+  })
+
+  // Button key listener
+  btn.addEventListener('keydown', e => {
+    // Key: t
+    if (e.key === 't') {
+      // Prevent default keydown
+      e.preventDefault()
+
+      // Spin that taco
+      btn.click()
+    }
+  })
 })
