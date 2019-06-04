58ca8979b2b1bf2a7b14757f383a2f397f937f06
kyleschaeffer <kyle@spirito.us>
2019-06-04 15:12:08 -0400

    Spin that taco

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+// Get the taco button
+const tacoBtn = document.getElementById('taco-btn')
+
+// Button click listener
+tacoBtn.addEventListener('click', () => {
+  tacoBtn.classList.toggle('spin')
+})
