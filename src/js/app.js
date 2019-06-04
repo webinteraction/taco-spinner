afa37184441980c81dfdd523e36772d5b6949ffc
kyleschaeffer <kyle@spirito.us>
2019-06-04 16:39:11 -0400

    Create Switch component class, add jiggle buttons

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,18 @@
+import { Switch } from './switch'
+
 // Add async button
 setTimeout(() => {
   document.getElementById('async-btn').innerHTML = `
-    <button id="pizza-btn" class="warning spin-btn">
+    <button id="pizza-btn" class="warning spin-btn" data-switch="spin">
       <span class="spinner">🍕</span>
     </button>
   `
 }, 3000)
 
-// Global click listener
-document.addEventListener('click', e => {
-  // Not a spin button
-  if (!e.target.matches('.spin-btn, .spin-btn *')) return
+// Switch buttons
+const switchBtns = new Switch()
 
-  // Prevent default click
-  e.preventDefault()
-
-  // Get the button
-  const btn = e.target.closest('.spin-btn')
-
-  // Spin that taco
-  btn.classList.toggle('spin')
-})
-
-// Global keydown listener
-document.addEventListener('keydown', e => {
-  // Not focused on spin button
-  if (!e.target.matches('.spin-btn')) return
-
-  // Key: t
-  if (e.key === 't') {
-    // Prevent default keydown
-    e.preventDefault()
-
-    // Get the button
-    const btn = e.target
-
-    // Spin that taco
-    btn.click()
-  }
+// Jiggle buttons
+const jiggleButtons = new Switch({
+  attr: 'data-jiggle',
 })
