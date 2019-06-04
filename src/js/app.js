ae01edf61103c519ac07c2695caa0bbd461930f8
kyleschaeffer <kyle@spirito.us>
2019-06-04 16:15:12 -0400

    Add global document listeners to support asynchronous content

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,41 @@
-// Iterate all spin buttons
-document.querySelectorAll('.spin-btn').forEach(btn => {
-  // Button click listener
-  btn.addEventListener('click', e => {
-    // Prevent default click
+// Add async button
+setTimeout(() => {
+  document.getElementById('async-btn').innerHTML = `
+    <button id="pizza-btn" class="warning spin-btn">
+      <span class="spinner">🍕</span>
+    </button>
+  `
+}, 3000)
+
+// Global click listener
+document.addEventListener('click', e => {
+  // Not a spin button
+  if (!e.target.matches('.spin-btn, .spin-btn *')) return
+
+  // Prevent default click
+  e.preventDefault()
+
+  // Get the button
+  const btn = e.target.closest('.spin-btn')
+
+  // Spin that taco
+  btn.classList.toggle('spin')
+})
+
+// Global keydown listener
+document.addEventListener('keydown', e => {
+  // Not focused on spin button
+  if (!e.target.matches('.spin-btn')) return
+
+  // Key: t
+  if (e.key === 't') {
+    // Prevent default keydown
     e.preventDefault()
 
+    // Get the button
+    const btn = e.target
+
     // Spin that taco
-    btn.classList.toggle('spin')
-  })
-
-  // Button key listener
-  btn.addEventListener('keydown', e => {
-    // Key: t
-    if (e.key === 't') {
-      // Prevent default keydown
-      e.preventDefault()
-
-      // Spin that taco
-      btn.click()
-    }
-  })
+    btn.click()
+  }
 })
