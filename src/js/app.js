7c1da4c3a8221908f0c167743073d80b7909d216
kyleschaeffer <kyle@spirito.us>
2019-06-04 15:34:06 -0400

    Prevent default, add keydown listener

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,22 @@
 const tacoBtn = document.getElementById('taco-btn')
 
 // Button click listener
-tacoBtn.addEventListener('click', () => {
+tacoBtn.addEventListener('click', e => {
+  // Prevent default click
+  e.preventDefault()
+
+  // Spin that taco
   tacoBtn.classList.toggle('spin')
+})
+
+// Button key listener
+tacoBtn.addEventListener('keydown', e => {
+  // Key: t
+  if (e.key === 't') {
+    // Prevent default keydown
+    e.preventDefault()
+
+    // Spin that taco
+    tacoBtn.click()
+  }
 })
